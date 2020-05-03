1128b46fde11e320d5cb6eb08c03079f99e24196
Daisuke Takahashi <daisuke@DaisukenoMacBook-Pro.local>
2020-05-03 13:39:33 +0900

    modify google_maps_api

--- scripts/google_maps_api.py
+++ scripts/google_maps_api.py
@@ -20,4 +20,18 @@ location = ["国会議事堂", "วัดพระแก้ว", "New York City"
 gmaps = googlemaps.Client(key=googleapikey)
 address = u'国会議事堂'
 result = gmaps.geocode(address)
-print(result[0]["geometry"])
+lat = result[0]['geometry']['location']['lat']
+lng = result[0]['geometry']['location']['lng']
+
+import requests
+
+lat = lat  # 緯度
+lon = lng # 経度
+
+url = "http://cyberjapandata2.gsi.go.jp/general/dem/scripts/getelevation.php" \
+       "?lon=%s&lat=%s&outtype=%s" %(lon, lat, "JSON")
+
+resp = requests.get(url, timeout=10)
+data = resp.json()
+
+print(data["elevation"])
