c6e5c4091e9e4ed95b1de5647e27b8fd260fc691
Daisuke Takahashi <daisuke@DaisukenoMacBook-Pro.local>
2020-05-09 01:53:47 +0900

    modify flask app

--- web-app/main/views.py
+++ web-app/main/views.py
@@ -1,32 +1,10 @@
-import flask
-from flask_googlemaps import Map
+from flask import render_template, request, send_file
 from main import app
 
-@app.route('/')
-def show_entries():
-    mymap = Map(
-        identifier="view-side",
-        lat=37.4419,
-        lng=-122.1419,
-        markers=[(37.4419, -122.1419)]
-    )
-    sndmap = Map(
-        identifier="sndmap",
-        lat=37.4419,
-        lng=-122.1419,
-        markers=[
-          {
-             'icon': 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
-             'lat': 37.4419,
-             'lng': -122.1419,
-             'infobox': "<b>Hello World</b>"
-          },
-          {
-             'icon': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
-             'lat': 37.4300,
-             'lng': -122.1400,
-             'infobox': "<b>Hello World from other place</b>"
-          }
-        ]
-    )
-    return flask.render_template('home.html', mymap=mymap, sndmap=sndmap)
+@app.route('/',methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        result = request.form
+        return send_file("static/routes_img.png", mimetype='image/png')
+    
+    return render_template('hello.html')
